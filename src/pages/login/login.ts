80e5b70b3ac5990a49ef23b5b45b376add0d3f2c
GlendyT <glendytuyuc@gmail.com>
2026-03-24 12:55:43 -0600

    adding login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  selector: "app-login",
+  imports: [FormsModule],
+  templateUrl: "./login.html",
+  styleUrl: "./login.css",
 })
-export class Login {}
+export class Login {
+  loginObj: any = {
+    email: "",
+    contactNo: "",
+  };
+
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  onLogin() {
+    debugger;
+    this.http
+      .post("http://localhost:5138/api/EmployeeMaster/login", this.loginObj)
+      .subscribe({
+        next: (result: any) => {
+          debugger;
+          localStorage.setItem("empLoginUser", JSON.stringify(result.data));
+          this.router.navigateByUrl("dashboard");
+        },
+        error: (error: any) => {
+          debugger;
+          alert(error.error.message);
+        },
+      });
+  }
+}
