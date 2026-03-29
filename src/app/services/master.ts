472cbb4f8f01aaebf1c2282bb7eda08c4c94a125
GlendyT <glendytuyuc@gmail.com>
2026-03-29 15:17:43 -0600

    adding to master.ts file the get all departments, save, and update

--- src/app/services/master.ts
+++ src/app/services/master.ts
@@ -1,6 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { DepartmentModal } from "../models/Department.models";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
-export class Master {}
+export class Master {
+  apiUrl: string = "http://localhost:5138/api/";
+  http = inject(HttpClient);
+
+  getAllDept() {
+    return this.http.get(this.apiUrl + "DepartmentMaster/GetAllDepartments");
+  }
+
+  saveDept(obj: DepartmentModal) {
+    return this.http.post(this.apiUrl + "DepartmentMaster/AddDepartment", obj);
+  }
+
+  updateDept(obj: DepartmentModal) {
+    return this.http.put(this.apiUrl + "DepartmentMaster/UpdateDepartment", obj);
+  }
+}
